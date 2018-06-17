0dbf7a68ff024da09815f990b28fc99ea67e7b03
SynthCannon <niksum15@gmail.com>
2018-06-17 15:55:00 +0100

    added client side

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AccountService } from './shared/account/account.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AccountListComponent } from './account-list/account-list.component';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccountListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
+  ],
+  providers: [AccountService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- server/src/main/java/com/angular/accounts/server/CoolAccountController.java
+++ server/src/main/java/com/angular/accounts/server/CoolAccountController.java
@@ -2,6 +2,7 @@ package com.angular.accounts.server;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,7 @@ class CoolAccountController {
     }
 
     @GetMapping("/cool-account")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Account> coolAccounts() {
         return repository.findAll().stream()
                 .filter(this::isCool)
